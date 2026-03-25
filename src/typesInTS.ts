d678f22669a482196236a922ed6fbc9b28b8f852
debabratabar <debabrata.bar1999@gmail.com>
2026-03-25 10:46:49 +0530

    chaicoxde-ts

---
+++ src/typesInTS.ts
@@ -0,0 +1,24 @@
+/**
+ * inference concept - when ts knows the data type of the variable on its own , usually every default data
+ * type in js , typescript can understand 
+ * 
+ * 
+ * 
+ * annotations concept - whern devs. have to annotate or explicitly tell what is the datatype 
+ * 
+ * 
+ * these are different style for declarations 
+ */
+
+
+
+let userName = 'coder_deb'
+
+
+// userName = 42
+
+
+let userName2 : string = 'coder_deb' 
+
+// userName2 = 42 
+
