406f143b822e54691b3ac923c7ed802cea1b7712
debabratabar <debabrata.bar1999@gmail.com>
2026-03-27 21:18:06 +0530

    practice-typescript

---
+++ src/typeNarrowDown.ts
@@ -0,0 +1,29 @@
+function chaiMake( kind : string | number){
+
+    // type narrowDown 
+    if ( typeof kind ==='string'){
+
+    }
+
+    return 
+}
+
+
+// we can also use type like this 
+
+let value : "approved" | 'rejected' | number ='approved'
+
+
+console.log(value);
+
+console.log(value);
+
+
+//custom types 
+type masala_maggi= { type : "masala" , price : number }
+
+
+let maggi : masala_maggi = { type : "masala" , price : 10}
+
+
+console.log(maggi);
