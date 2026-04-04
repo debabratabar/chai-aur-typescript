c552a0f7a023ace62905d06f04b356e5a516682c
debabratabar <debabrata.bar1999@gmail.com>
2026-04-04 11:05:35 +0530

    practice-webreq

--- src/tsGenerics.ts
+++ src/tsGenerics.ts
@@ -1,4 +1,28 @@
 // simple code example 
+//helping devs to create n
+
+
+function wrapInArry<T>(item : T) : T[] { 
+    if ( Array.isArray(item)){// type gurding
+            return [...item]
+    }
+
+    return [item]
+    
+}
+
+
+console.log(wrapInArry(['chaicode','yntp']));
+
+
+
+function swap<A,B>( a : A , b : B) : [B , A] {
+    return [b,a]
+
+}
+
+
+console.log(swap(1,2 ));
 
 
 
